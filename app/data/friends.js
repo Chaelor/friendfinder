a0e79e55843eec8a50a37b4f25ae9aa3bc12d41b
Chaelor <joshsblizzard@gmail.com>
2018-09-04 14:11:33 -0500

    Friends json array done

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friendsArray = 
+[
+    {
+        "name": "John Jackson",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1Mzg2Njg2OTkx/portlandia-season-2-premiere-screening.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            3,
+            2,
+            1,
+            5,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Sarah Kerrigan",
+        "photo": "https://vignette.wikia.nocookie.net/vsbattles/images/9/90/Ghost_Kerrigan.jpg/revision/latest/scale-to-width-down/1000?cb=20161221081121",
+        "scores": [
+            4,
+            5,
+            3,
+            4,
+            1,
+            3,
+            4,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Patrick Star",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png",
+        "scores": [
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5,
+            1
+        ]
+    }
+];
